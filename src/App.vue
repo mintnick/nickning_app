5436524af196b312e9ae3211f8670b95c98382c4
Nick Ning <mintnick@gmail.com>
2024-01-08 14:27:52 +0800

    add: language component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <script setup>
 import LanguagesBtnGroup from './components/LanguagesBtnGroup.vue'
-
-// vue cookie
-import { inject } from 'vue'
-const $cookies = inject('$cookies');
-
-// i18n
-import { useI18n } from 'vue-i18n';
-const i18n = useI18n();
+import Section from './components/Section.vue'
 
 // load lang from cookie
+import { inject } from 'vue'
+const $cookies = inject('$cookies');
+import { useI18n } from 'vue-i18n';
+const i18n = useI18n();
 i18n.locale.value = $cookies.get('lang') ?? 'en';
+
+// load projects
+import projects from './assets/projects.json'
+
 </script>
 
 <template>
   <LanguagesBtnGroup />
 
-  <div>
-    <img src="./assets/images/logos/N-transparent.png" />
+  <div id="header">
+    <img src="/images/logos/N-transparent.png" />
     <h1>{{ $t('site_title') }}</h1>
   </div>
 
-
+  <Section v-for="project in projects" 
+  :section_name="project.section_name" :section_logo="project.section_logo" :items="project.items"/>
 </template>
 
 <style scoped>
+#header > img {
+  height: 120px;
+}
 </style>
---
+++ src/components/Section.vue
@@ -0,0 +1,7 @@
+<script setup>
+const props = defineProps(["section_name", "section_logo", "items"]);
+</script>
+
+<template>
+  <h2>{{ $t(section_name) }}</h2>
+</template>
