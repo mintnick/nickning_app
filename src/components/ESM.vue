8c04c9a0728e5b199c4276e97f117acf3890eede
Nick Ning <mintnick@gmail.com>
2024-01-20 21:03:57 +0800

    upgrade: yarn pnp

--- src/components/ESM.vue
+++ src/components/ESM.vue
@@ -4,9 +4,14 @@
 <template>
   <div class="text-grey-darken-3">
     <h1 class="text-center">{{ $t('esm') }}</h1>
-    <h2>{{ $t('intro') }}</h2>
     <h2>{{ $t('download') }}</h2>
-    <h2>{{ $t('usage') }}</h2>
+    <ul>
+      <li>123</li>
+      <li>123</li>
+      <li>123</li>
+    </ul>
+    <h2>{{ $t('intro') }}</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit accusamus harum unde iure, officiis, quasi aliquid est et laudantium, cum adipisci magni. Reprehenderit laborum sed provident libero itaque ipsum sunt cupiditate facere adipisci minus, maxime fugiat repudiandae ea natus non! Facere totam suscipit voluptate soluta saepe molestias non natus doloribus.</p>
     <h2>{{ $t('support') }}</h2>
   </div>
 </template>
