f2ebb1210f7b7da38ff05c7f3832801d9ee90c98
Nick Ning <mintnick@gmail.com>
2024-01-27 09:56:37 +0800

    add: dotenv and outdir. style: screenshot max height and width

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,6 +7,7 @@ import ViteFonts from 'unplugin-fonts/vite'
 // Utilities
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
+import 'dotenv/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -44,5 +45,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: '',
+  // base: '',
+  build: {
+    outDir: process.env.OUTDIR,
+    emptyOutDir: true,
+  }
 })
